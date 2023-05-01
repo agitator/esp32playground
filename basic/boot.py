ab8b9219af022b8e35f44c457b1b7fb8d31ff691
Peter Holzer <peter.holzer@agitator.com>
2023-05-01 20:07:41 +0200

    connect to network if env.json available

--- basic/boot.py
+++ basic/boot.py
@@ -1,1 +1,18 @@
 # boot.py -- run on boot-up
+ENV = False
+
+try:
+    import os
+    from mpy_env import load_env, get_env
+    if "env.json" in os.listdir():
+        load_env(verbose=True)
+        ENV = True
+        print("env.json found and loaded")
+    else:
+        print("No env.json found for network setup")
+except Exception as e:
+    print("Could not setup env: {}".format(e))
+
+if ENV:
+    from utils import do_connect
+    do_connect(get_env("WIFI_SSID"), get_env("WIFI_PASSWD"))
