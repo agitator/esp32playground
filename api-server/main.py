40626f474b6d2a966cdeb1b760382ff62755b199
Peter Holzer <peter.holzer@agitator.com>
2023-07-23 18:00:03 +0200

    add microdot and hello world

--- api-server/main.py
+++ api-server/main.py
@@ -1,3 +1,36 @@
 # main.py -- put your code here!
+print("API Server with Microdot here")
 
-print("API Server here")
+from microdot import Microdot
+
+app = Microdot()
+
+htmldoc = '''<!DOCTYPE html>
+<html>
+    <head>
+        <title>Microdot Example Page</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div>
+            <h1>Microdot Example Page</h1>
+            <p>Hello from Microdot!</p>
+            <p><a href="/shutdown">Click to shutdown the server</a></p>
+        </div>
+    </body>
+</html>
+'''
+
+
+@app.route('/')
+def hello(request):
+    return htmldoc, 200, {'Content-Type': 'text/html'}
+
+
+@app.route('/shutdown')
+def shutdown(request):
+    request.app.shutdown()
+    return 'The server is shutting down...'
+
+
+app.run(debug=True)
